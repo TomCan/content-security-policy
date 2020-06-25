c804793c7ebf4cc6a4ce3a7aa16fe207765e703d
Tom Cannaerts <tom.cannaerts@intracto.com>
2020-06-25 22:10:56 +0200

    Rewrite tests from array to object

--- tests/Parser/ParserTest.php
+++ tests/Parser/ParserTest.php
@@ -3,6 +3,7 @@
 namespace Test\Parser;
 
 use PHPUnit\Framework\TestCase;
+use TomCan\Csp\ContentSecurityPolicy;
 use TomCan\Csp\Exception\CspInvalidDirectiveException;
 use TomCan\Csp\CspParser;
 use TomCan\Csp\Exception\CspInvalidSourceListItemException;
@@ -13,17 +14,15 @@ class ParserTest extends TestCase
     {
         $parser = new CspParser();
         $result = $parser->parse("");
-        $this->assertIsArray($result);
-        $this->assertCount(0, $result);
+        $this->assertCount(0, $result->getDirectives());
     }
 
     public function testBasicWithoutHeader(): void
     {
         $parser = new CspParser();
-        $result = $parser->parse('default-src tom');
-        $this->assertIsArray($result);
-        $this->assertCount(1, $result);
-        $this->assertArrayHasKey('default-src', $result);
+        $result = $parser->parse('default-src https://www.tom.be');
+        $this->assertCount(1, $result->getDirectives());
+        $this->assertArrayHasKey(ContentSecurityPolicy::DIRECTIVE_DEFAULT_SRC, $result->getDirectives());
     }
 
     public function testBasicWithHeader(): void
@@ -36,10 +35,9 @@ class ParserTest extends TestCase
             "CoNtEnT-SeCuRiTy-PoLiCy: ",
         ];
         foreach ($headers as $header) {
-            $result = $parser->parse($header . 'default-src tom');
-            $this->assertIsArray($result);
-            $this->assertCount(1, $result);
-            $this->assertArrayHasKey('default-src', $result);
+            $result = $parser->parse($header . 'default-src https://www.tom.be');
+            $this->assertCount(1, $result->getDirectives());
+            $this->assertArrayHasKey(ContentSecurityPolicy::DIRECTIVE_DEFAULT_SRC, $result->getDirectives());
         }
     }
 
@@ -47,20 +45,15 @@ class ParserTest extends TestCase
     {
         $parser = new CspParser();
 
-        $result = $parser->parse( 'Content-Security-Policy: default-src tom');
-        $this->assertIsArray($result);
-        $this->assertCount(1, $result);
-        $this->assertArrayHasKey('default-src', $result);
-        $this->assertArrayNotHasKey('report', $result);
+        $result = $parser->parse( 'Content-Security-Policy: default-src https://www.tom.be');
+        $this->assertCount(1, $result->getDirectives());
+        $this->assertArrayHasKey(ContentSecurityPolicy::DIRECTIVE_DEFAULT_SRC, $result->getDirectives());
+        $this->assertFalse($result->isReportOnly());
 
-        $result = $parser->parse( 'Content-Security-Policy-Report-Only: default-src tom');
-        $this->assertIsArray($result);
-        $this->assertCount(2, $result);
-        $this->assertArrayHasKey('default-src', $result);
-        $this->assertArrayHasKey('report', $result);
-        $this->assertArrayHasKey('report-only', $result['report']);
-        $this->assertTrue($result['report']['report-only']);
-
+        $result = $parser->parse( 'Content-Security-Policy-Report-Only: default-src https://www.tom.be');
+        $this->assertCount(1, $result->getDirectives());
+        $this->assertArrayHasKey(ContentSecurityPolicy::DIRECTIVE_DEFAULT_SRC, $result->getDirectives());
+        $this->assertTrue($result->isReportOnly());
     }
 
     public function testBasicDirectives(): void
@@ -80,49 +73,54 @@ class ParserTest extends TestCase
 
         $parser = new CspParser();
         foreach ($directivesToTest as $directiveToTest) {
-            $result = $parser->parse($directiveToTest.' tom');
-            $this->assertIsArray($result);
-            $this->assertCount(1, $result);
-            $this->assertArrayHasKey($directiveToTest, $result);
+            $result = $parser->parse($directiveToTest.' https://www.tom.be');
+            $this->assertCount(1, $result->getDirectives());
+            $this->assertArrayHasKey($directiveToTest, $result->getDirectives());
         }
     }
 
     public function testMultipleDirectives(): void
     {
         $parser = new CspParser();
-        $result = $parser->parse('default-src tom;script-src \'self\'');
-        $this->assertCount(2, $result);
-        $this->assertArrayHasKey('default-src', $result);
-        $this->assertArrayHasKey('script-src', $result);
+        $result = $parser->parse('default-src https://www.tom.be;script-src \'self\'');
+        $this->assertCount(2, $result->getDirectives());
+        $this->assertArrayHasKey('default-src', $result->getDirectives());
+        $this->assertArrayHasKey('script-src', $result->getDirectives());
 
-        $result = $parser->parse('default-src tom ; script-src \'self\' ;');
-        $this->assertCount(2, $result);
-        $this->assertArrayHasKey('default-src', $result);
-        $this->assertArrayHasKey('tom', $result['default-src']);
-        $this->assertArrayHasKey('script-src', $result);
-        $this->assertArrayHasKey('\'self\'', $result['script-src']);
+        $result = $parser->parse('default-src https://www.tom.be ; script-src \'self\' ;');
+        $this->assertCount(2, $result->getDirectives());
+        $this->assertArrayHasKey('default-src', $result->getDirectives());
+        $this->assertArrayHasKey('script-src', $result->getDirectives());
 
+        $result = $parser->parse('default-src https://www.tom.be ; ;; ; ;; ;; ;; ;script-src \'self\' ;');
+        $this->assertCount(2, $result->getDirectives());
+        $this->assertArrayHasKey('default-src', $result->getDirectives());
+        $this->assertArrayHasKey('script-src', $result->getDirectives());
     }
 
     public function testInvalidDirective(): void
     {
         $this->expectException(CspInvalidDirectiveException::class);
         $parser = new CspParser();
-        $result = $parser->parse('invalid-src tom');
+        $result = $parser->parse('invalid-src https://www.tom.be');
     }
 
     public function testPredefinedValues(): void
     {
         $parser = new CspParser();
-        $result = $parser->parse("default-src 'none' 'self'");
-        $this->assertCount(2, $result['default-src']);
-        $result = $parser->parse("style-src 'none' 'self' 'unsafe-inline'");
-        $this->assertCount(3, $result['style-src']);
-        $this->assertArrayHasKey("'unsafe-inline'", $result['style-src']);
-        $result = $parser->parse("script-src 'none' 'self' 'unsafe-inline' 'unsafe-eval'");
-        $this->assertCount(4, $result['script-src']);
-        $this->assertArrayHasKey("'unsafe-inline'", $result['script-src']);
-        $this->assertArrayHasKey("'unsafe-eval'", $result['script-src']);
+        $result = $parser->parse("default-src 'none'");
+        $this->assertCount(1, $result->getDirective('default-src'));
+
+        $result = $parser->parse("default-src 'self'");
+        $this->assertCount(1, $result->getDirective('default-src'));
+
+        $result = $parser->parse("style-src 'self' 'unsafe-inline'");
+        $this->assertCount(2, $result->getDirective('style-src'));
+        $this->assertArrayHasKey("'unsafe-inline'", $result->getDirective('style-src'));
+        $result = $parser->parse("script-src 'self' 'unsafe-inline' 'unsafe-eval'");
+        $this->assertCount(3, $result->getDirective('script-src'));
+        $this->assertArrayHasKey("'unsafe-inline'", $result->getDirective('script-src'));
+        $this->assertArrayHasKey("'unsafe-eval'", $result->getDirective('script-src'));
     }
 
     public function testInvalidPredefinedValuesStrict(): void
@@ -130,75 +128,86 @@ class ParserTest extends TestCase
         $parser = new CspParser();
         $this->expectException(CspInvalidSourceListItemException::class);
         $result = $parser->parse("default-src 'self' 'invalid'");
+
         $this->expectException(CspInvalidSourceListItemException::class);
         $result = $parser->parse("default-src 'self' 'unsafe-inline'");
     }
 
     public function testInvalidPredefinedValuesLoose(): void
     {
-        $parser = new CspParser(CspParser::MODE_LOOSE);
+        $parser = new CspParser(ContentSecurityPolicy::MODE_LOOSE);
         $result = $parser->parse("default-src 'self' 'invalid'");
-        $this->assertCount(1, $result['default-src']);
+        $this->assertCount(1, $result->getDirective('default-src'));
+
         $result = $parser->parse("default-src 'self' 'unsafe-inline'");
-        $this->assertCount(1, $result['default-src']);
+        $this->assertCount(1, $result->getDirective('default-src'));
+
         $result = $parser->parse("style-src 'self' 'unsafe-inline'");
-        $this->assertCount(2, $result['style-src']);
-        $this->assertArrayHasKey("'unsafe-inline'", $result['style-src']);
+        $this->assertCount(2, $result->getDirective('style-src'));
+        $this->assertArrayHasKey("'unsafe-inline'", $result->getDirective('style-src'));
+
         $result = $parser->parse("script-src 'self' 'unsafe-inline' 'unsafe-eval'");
-        $this->assertCount(3, $result['script-src']);
-        $this->assertArrayHasKey("'unsafe-inline'", $result['script-src']);
-        $this->assertArrayHasKey("'unsafe-eval'", $result['script-src']);
+        $this->assertCount(3, $result->getDirective('script-src'));
+        $this->assertArrayHasKey("'unsafe-inline'", $result->getDirective('script-src'));
+        $this->assertArrayHasKey("'unsafe-eval'", $result->getDirective('script-src'));
     }
 
     public function testSandbox(): void
     {
         $parser = new CspParser();
         $result = $parser->parse('sandbox');
-        $this->assertCount(1, $result);
-        $this->assertCount(0, $result['sandbox']);
+        $this->assertCount(0, $result->getDirective('sandbox'));
+
         $result = $parser->parse('sandbox allow-forms');
-        $this->assertCount(1, $result);
-        $this->assertCount(1, $result['sandbox']);
+        $this->assertCount(1, $result->getDirective('sandbox'));
+
         $result = $parser->parse('sandbox allow-forms allow-same-origin allow-scripts allow-popups allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-popups-to-escape-sandbox allow-top-navigation');
-        $this->assertCount(10, $result['sandbox']);
+        $this->assertCount(10, $result->getDirective('sandbox'));
+        $this->assertArrayHasKey('allow-forms', $result->getDirective('sandbox'));
+        $this->assertArrayHasKey('allow-orientation-lock', $result->getDirective('sandbox'));
+
         $this->expectException(CspInvalidSourceListItemException::class);
         $result = $parser->parse('sandbox invalid');
     }
 
     public function testNonceL2(): void
     {
-        $parser = new CspParser(CspParser::MODE_STRICT, 2);
+        $parser = new CspParser(ContentSecurityPolicy::MODE_STRICT, 2);
         $result = $parser->parse("style-src 'self' 'nonce-dmFsaWQgbm9uY2U='");
-        $this->assertArrayHasKey("'nonce-dmFsaWQgbm9uY2U='", $result['style-src']);
+        $this->assertArrayHasKey("'nonce-dmFsaWQgbm9uY2U='", $result->getDirective('style-src'));
+
         $result = $parser->parse("style-src 'self' 'nonce-not+valid+base64+but+valid+enough'");
-        $this->assertArrayHasKey("'nonce-not+valid+base64+but+valid+enough'", $result['style-src']);
+        $this->assertArrayHasKey("'nonce-not+valid+base64+but+valid+enough'", $result->getDirective('style-src'));
+
         $this->expectException(CspInvalidSourceListItemException::class);
         $result = $parser->parse("style-src 'self' 'nonce-inv#alid'");
     }
 
     public function testNonceL1(): void
     {
-        $parser = new CspParser(CspParser::MODE_STRICT, 1);
+        $parser = new CspParser(ContentSecurityPolicy::MODE_STRICT, 1);
         $this->expectException(CspInvalidSourceListItemException::class);
         $result = $parser->parse("style-src 'self' 'nonce-dmFsaWQgbm9uY2U='");
     }
 
     public function testShaL2(): void
     {
-        $parser = new CspParser(CspParser::MODE_STRICT, 2);
+        $parser = new CspParser(ContentSecurityPolicy::MODE_STRICT, 2);
         foreach (['sha256', 'sha384', 'sha512'] as $sha) {
             $result = $parser->parse("style-src '".$sha."-dmFsaWQgbm9uY2U='");
-            $this->assertArrayHasKey("'".$sha."-dmFsaWQgbm9uY2U='", $result['style-src']);
+            $this->assertArrayHasKey("'".$sha."-dmFsaWQgbm9uY2U='", $result->getDirective('style-src'));
         }
+
         $this->expectException(CspInvalidSourceListItemException::class);
         $result = $parser->parse("style-src 'self' 'sha666-dmFsaWQgbm9uY2U=");
+
         $this->expectException(CspInvalidSourceListItemException::class);
         $result = $parser->parse("style-src 'self' 'sha256-inv#alid'");
     }
 
     public function testShaL1(): void
     {
-        $parser = new CspParser(CspParser::MODE_STRICT, 1);
+        $parser = new CspParser(ContentSecurityPolicy::MODE_STRICT, 1);
         $this->expectException(CspInvalidSourceListItemException::class);
         $result = $parser->parse("style-src 'self' 'sha256-dmFsaWQgbm9uY2U='");
     }
