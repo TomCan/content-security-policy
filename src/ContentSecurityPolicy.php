f7c37b1dbb61540c5851c0688931f5bb2b04b9e1
Tom Cannaerts <tom.cannaerts@intracto.com>
2020-07-06 19:42:19 +0200

    Re-order

--- src/ContentSecurityPolicy.php
+++ src/ContentSecurityPolicy.php
@@ -10,66 +10,66 @@ use TomCan\Csp\Exception\CspInvalidSourceListItemException;
 class ContentSecurityPolicy
 {
 
-    const DIRECTIVE_DEFAULT_SRC = 'default-src';
-    const DIRECTIVE_SCRIPT_SRC = 'script-src';
-    const DIRECTIVE_STYLE_SRC = 'style-src';
-    const DIRECTIVE_IMG_SRC = 'img-src';
-    const DIRECTIVE_FONT_SRC = 'font-src';
+    const DIRECTIVE_BASE_URI = 'base-uri';
     const DIRECTIVE_CONNECT_SRC = 'connect-src';
+    const DIRECTIVE_DEFAULT_SRC = 'default-src';
+    const DIRECTIVE_FONT_SRC = 'font-src';
+    const DIRECTIVE_FRAME_SRC = 'frame-src';
+    const DIRECTIVE_IMG_SRC = 'img-src';
+    const DIRECTIVE_MANIFEST_SRC = 'manifest-src';
     const DIRECTIVE_MEDIA_SRC = 'media-src';
     const DIRECTIVE_OBJECT_SRC = 'object-src';
-    const DIRECTIVE_FRAME_SRC = 'frame-src';
+    const DIRECTIVE_PREFETCH_SRC = 'prefetch-src';
     const DIRECTIVE_REPORT_TO = 'report-to';
     const DIRECTIVE_REPORT_URI = 'report-uri';
-    const DIRECTIVE_BASE_URI = 'base-uri';
-    const DIRECTIVE_WORKER_SRC = 'worker-src';
-    const DIRECTIVE_MANIFEST_SRC = 'manifest-src';
-    const DIRECTIVE_PREFETCH_SRC = 'prefetch-src';
     const DIRECTIVE_SANDBOX = 'sandbox';
+    const DIRECTIVE_SCRIPT_SRC = 'script-src';
+    const DIRECTIVE_STYLE_SRC = 'style-src';
+    const DIRECTIVE_WORKER_SRC = 'worker-src';
 
     const VALID_DIRECTIVES = [
-        self::DIRECTIVE_DEFAULT_SRC,
-        self::DIRECTIVE_SCRIPT_SRC,
-        self::DIRECTIVE_STYLE_SRC,
-        self::DIRECTIVE_IMG_SRC,
-        self::DIRECTIVE_FONT_SRC,
+        self::DIRECTIVE_BASE_URI,
         self::DIRECTIVE_CONNECT_SRC,
+        self::DIRECTIVE_DEFAULT_SRC,
+        self::DIRECTIVE_FONT_SRC,
+        self::DIRECTIVE_FRAME_SRC,
+        self::DIRECTIVE_IMG_SRC,
+        self::DIRECTIVE_MANIFEST_SRC,
         self::DIRECTIVE_MEDIA_SRC,
         self::DIRECTIVE_OBJECT_SRC,
-        self::DIRECTIVE_FRAME_SRC,
+        self::DIRECTIVE_PREFETCH_SRC,
         self::DIRECTIVE_REPORT_TO,
         self::DIRECTIVE_REPORT_URI,
-        self::DIRECTIVE_BASE_URI,
-        self::DIRECTIVE_WORKER_SRC,
-        self::DIRECTIVE_MANIFEST_SRC,
-        self::DIRECTIVE_PREFETCH_SRC,
         self::DIRECTIVE_SANDBOX,
+        self::DIRECTIVE_SCRIPT_SRC,
+        self::DIRECTIVE_STYLE_SRC,
+        self::DIRECTIVE_WORKER_SRC,
     ];
 
     const MODE_STRICT = 0;
     const MODE_LOOSE = 1;
 
     const SANDBOX_ALLOW_FORMS = 'allow-forms';
-    const SANDBOX_ALLOW_SAME_ORIGIN = 'allow-same-origin';
-    const SANDBOX_ALLOW_SCRIPTS = 'allow-scripts';
-    const SANDBOX_ALLOW_POPUPS = 'allow-popups';
     const SANDBOX_ALLOW_MODALS = 'allow-modals';
     const SANDBOX_ALLOW_ORIENTATION_LOCK = 'allow-orientation-lock';
     const SANDBOX_ALLOW_POINTER_LOCK = 'allow-pointer-lock';
-    const SANDBOX_ALLOW_PRESENTATION = 'allow-presentation';
+    const SANDBOX_ALLOW_POPUPS = 'allow-popups';
     const SANDBOX_ALLOW_POPUPS_TO_ESCAPE_SANDBOX = 'allow-popups-to-escape-sandbox';
+    const SANDBOX_ALLOW_PRESENTATION = 'allow-presentation';
+    const SANDBOX_ALLOW_SAME_ORIGIN = 'allow-same-origin';
+    const SANDBOX_ALLOW_SCRIPTS = 'allow-scripts';
     const SANDBOX_ALLOW_TOP_NAVIGATION = 'allow-top-navigation';
 
     const VALID_SANDBOX_OPTIONS = [
         self::SANDBOX_ALLOW_FORMS,
-        self::SANDBOX_ALLOW_SAME_ORIGIN,
-        self::SANDBOX_ALLOW_SCRIPTS,
-        self::SANDBOX_ALLOW_POPUPS,
         self::SANDBOX_ALLOW_MODALS,
         self::SANDBOX_ALLOW_ORIENTATION_LOCK,
         self::SANDBOX_ALLOW_POINTER_LOCK,
-        self::SANDBOX_ALLOW_PRESENTATION,
+        self::SANDBOX_ALLOW_POPUPS,
         self::SANDBOX_ALLOW_POPUPS_TO_ESCAPE_SANDBOX,
+        self::SANDBOX_ALLOW_PRESENTATION,
+        self::SANDBOX_ALLOW_SAME_ORIGIN,
+        self::SANDBOX_ALLOW_SCRIPTS,
         self::SANDBOX_ALLOW_TOP_NAVIGATION,
     ];
 
@@ -77,14 +77,14 @@ class ContentSecurityPolicy
     // basic matching, need at least 1 character, ending in 0, 1 or 2 =
     const PAT_BASE64 = '[A-Za-z0-9+\/]+={0,2}';
 
+    const PAT_SOURCE_NONCE = "'nonce-".self::PAT_BASE64."'";
     const PAT_SOURCE_NONE = "'none'";
     const PAT_SOURCE_SELF = "'self'";
-    const PAT_SOURCE_UNSAFE_EVAL = "'unsafe-eval'";
-    const PAT_SOURCE_UNSAFE_INLINE = "'unsafe-inline'";
     const PAT_SOURCE_SHA = "'sha(256|384|512)-".self::PAT_BASE64."'";
-    const PAT_SOURCE_NONCE = "'nonce-".self::PAT_BASE64."'";
     const PAT_SOURCE_STRICT_DYNAMIC = "'strict-dynamic'";
+    const PAT_SOURCE_UNSAFE_EVAL = "'unsafe-eval'";
     const PAT_SOURCE_UNSAFE_HASHES = "'unsafe-hashes'";
+    const PAT_SOURCE_UNSAFE_INLINE = "'unsafe-inline'";
 
     const OUTPUT_FULL_HEADER = 0;
     const OUTPUT_VALUE_ONLY = 1;
